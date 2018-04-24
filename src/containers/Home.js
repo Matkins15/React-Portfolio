e55cfb84680fada9a97787d2dc323858331ede83
Matthew Atkins <matkinsdesigns@gmail.com>
2018-04-24 12:44:15 -0400

    added Link component to nav anchor

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,20 +1,41 @@
 import React, { Component, PropTypes } from 'react'
 import styled from 'styled-components'
 import Container from '../theme/layout/Container'
+import { Link } from 'react-router';
+
 
 const Image = styled.img`
-  width: 100%;
+  width: 100vw;
+  height: 80vh; 
  `
+
+ const Nav = styled.div`
+   width: 100vw;
+   height: 10vh;
+   display: flex;
+   flex-direction: row;
+   justify-content: center;
+   align-items: center;
+ `
+ 
 
 export default class Home extends Component {
   static PropTypes = {}
 
   render () {
     return (
-      <Container>
+      <div>
+        <Nav>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link>Portfolio</Link>
+          <Link>Contact</Link>
+        </Nav>
         <Image src={require('../assets/music-store.jpg')} alt='music-store'/>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </Container>
+        <Container>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </Container>
+      </div>
     )
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { IndexRoute, Router, Route, hashHistory } from 'react-router'
 import Home from './containers/Home'
+import About from './containers/About'
 import App from './containers/App'
 
 export default (
   <Router history={hashHistory}>
     <Route path='/' component={App}>
       <IndexRoute component={Home} />
+    </Route>
+    <Route path='/about' component={App}>
+      <IndexRoute component={About} />
     </Route>
   </Router>
 )
