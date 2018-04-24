5f32e499fa391f62aeecba38a9e650824ddc4669
Matthew Atkins <matkinsdesigns@gmail.com>
2018-04-23 23:18:52 -0400

    home image and text

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,20 @@
 import React, { Component, PropTypes } from 'react'
+import styled from 'styled-components'
+import Container from '../theme/layout/Container'
+
+const Image = styled.img`
+  width: 100%;
+ `
 
 export default class Home extends Component {
   static PropTypes = {}
 
   render () {
     return (
-      <div>
-        <h1>Hi Home</h1>
-      </div>
-    )  
+      <Container>
+        <Image src={require('../assets/music-store.jpg')} alt='music-store'/>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </Container>
+    )
   }
 }
---
+++ src/theme/globalStyles.js
@@ -0,0 +1,11 @@
+import { injectGlobal } from 'styled-components'
+
+/* eslint-disable */
+injectGlobal`
+@import url('https://fonts.googleapis.com/css?family=Economica');
+
+body {
+  font-family: Economica, sans-serif;
+  font-size: 24px;
+}
+`
