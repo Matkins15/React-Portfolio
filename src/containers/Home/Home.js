a05c4512efaebddc84903c12f9f57e264e98cbff
Matthew Atkins <matkinsdesigns@gmail.com>
2018-05-02 01:28:20 -0400

    created canvas component and added particles.js plugin

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import { Container } from '../../theme/layout'
 import Nav from '../../containers/Nav/Nav'
 import { LandingImage, RevealP } from './Home.style'
-// import WhenInView from 'components/WhenInView/WhenInView'
+import Canvas from '../../containers/Canvas/Canvas.js'
 
 class Home extends Component {
 
@@ -11,13 +11,9 @@ class Home extends Component {
       <div>
         <Nav />
         <Container>
-          <LandingImage>
-            <h1>Matthew Atkins</h1>
-            <h4>Front End Developer</h4>
-          </LandingImage>
-          <RevealP hide={false}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </RevealP>
+          <Canvas />
+          <h1>Matthew Atkins</h1>
+          <h4>Front End Developer</h4>
         </Container>
       </div>
     )
