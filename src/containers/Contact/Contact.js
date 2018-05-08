79f830947dadda90da3a644ecb1acb619a2cc2fe
Matthew Atkins <matkinsdesigns@gmail.com>
2018-05-08 11:57:25 -0400

    added social links dynamically to on click function

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -3,36 +3,23 @@ import { Container } from '../../theme/layout'
 import { Wrapper, SocialIcons } from './Contact.style'
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
-import { FaBeer, FaGithub, FaInstagram, FaLinkedin, FaEnvelope} from 'react-icons/lib/fa';
+import { FaBeer, FaGithub, FaInstagram, FaLinkedin, FaEnvelope} from 'react-icons/lib/fa'
 
 class Contact extends Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
     this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      message: ''
+      instagramShare: 'https://www.instagram.com/matkins15/',
+      githubShare: 'https://github.com/Matkins15',
+      linkedinShare: 'https://www.linkedin.com/in/matthewatkins/'
     }
+    console.log(this.state)
   }
 
-  change = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
+  openConnection = (media) => {
+    window.open(media)
   }
 
-  onSubmit = (e) => {
-    e.preventDefault()
-    console.log(this.state);
-    this.setState({
-      firstName: '',
-      lastName: '',
-      email: '',
-      subject: '' 
-    })
-  }
-  
   render () {
     const buttonStyle = {
       display: 'table',
@@ -48,10 +35,10 @@ class Contact extends Component {
         <Wrapper>
           <h3>Hit me up!</h3>
           <SocialIcons>
-            <FaGithub style={social}/>
-            <FaInstagram style={social}/>
+            <FaGithub style={social} onClick={() => this.openConnection(this.state.githubShare)} target="_blank"  />
+            <FaInstagram style={social} onClick={() => this.openConnection(this.state.instagramShare)} target="_blank" />
             <FaEnvelope style={social}/>
-            <FaLinkedin style={social}/>
+            <FaLinkedin style={social} onClick={() => this.openConnection(this.state.linkedinShare)} target="_blank" />
           </SocialIcons>
         </Wrapper>
       </Container>
